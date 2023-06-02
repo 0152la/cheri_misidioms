b6da033fae5d9fd1573d81fe2f2e1c2b07b281ff
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2023-06-02 14:40:39 +0100

    Fix

--- data/get_all.py
+++ data/get_all.py
@@ -303,10 +303,16 @@ class Allocator:
         return lib_file_path
 
     def get_static_libfile(self, mode):
-        assert(self.install_mode == InstallMode.REPO)
-        static_lib_path = self.raw_data['install']['lib_file_static']
-        if not os.path.isabs(static_lib_path):
-            static_lib_path = os.path.join(self.source_path, static_lib_path)
+        if self.install_mode == InstallMode.REPO:
+            static_lib_path = self.raw_data['install']['lib_file_static']
+            if not os.path.isabs(static_lib_path):
+                static_lib_path = os.path.join(self.source_path, static_lib_path)
+        elif self.install_mode == InstallMode.CHERIBUILD:
+            static_lib_path = self.raw_data['install'][mode]['lib_file_static']
+            if not os.path.isabs(static_lib_path):
+                static_lib_path = os.path.join(self.source_path, static_lib_path)
+        else:
+            assert(False)
         return static_lib_path
 
     def get_raw_libfile(self):
@@ -534,6 +540,8 @@ def prepare_benchs(bench_sources, machine, static_alloc = None):
             machine_dest_dir = os.path.join(machine.get_work_dir(mode), f"static-{mode}-{static_alloc.name}")
             lib = "static"
             cmake_config_cmd = f"{cmake_config_cmd} -Dstaticlib={static_alloc.get_static_libfile(mode)}"
+            if "static_ld_flags" in static_alloc.raw_data["install"]:
+                cmake_config_cmd = f"{cmake_config_cmd} -DCFLAGS='{static_alloc.raw_data['install']['static_ld_flags']}'"
         else:
             dest = os.path.join(work_dir_local, f"benchs-{mode}")
             machine_dest_dir = machine.get_work_dir(mode)
@@ -558,31 +566,6 @@ def prepare_benchs(bench_sources, machine, static_alloc = None):
             new_bench.add_path("remote", mode, paths["remote_path"])
         bench_objs.append(new_bench)
     return bench_objs
-
-def prepare_benchs_static(bench_sources, machine, alloc):
-    assert(os.path.exists(bench_sources))
-    cmake_config_cmd = """cmake -S {source} -B {dest}/build
-    -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={dest}/install
-    -Dgclib={lib} -Dbm_logfile=out.json -DSDK={sdk}
-    -DCMAKE_TOOLCHAIN_FILE={toolchain}"""
-    benchs = []
-    for mode in benchmark_modes.keys():
-        dest = os.path.join(work_dir_local, f"static-benchs-{mode}-{alloc.name}")
-        lib = alloc.get_static_libfile(mode)
-        subprocess.check_call(shlex.split(
-            cmake_config_cmd.format(source = bench_sources, dest = dest, lib = lib,
-                sdk = os.path.join(work_dir_local, "cheribuild", "output", "morello-sdk"),
-                toolchain = os.path.join(bench_sources, f"morello-{mode}.cmake"))))
-        subprocess.check_call(shlex.split(f"cmake --build {os.path.join(dest, 'build')}"))
-        subprocess.check_call(shlex.split(f"cmake --install {os.path.join(dest, 'build')}"))
-        for dir_path, _, new_bench_files in os.walk(os.path.join(dest, "install")):
-            for filn in new_bench_files:
-                if filn.endswith(".elf"):
-                    benchs.append(filn)
-                dest_dir = os.path.join(machine.get_work_dir(mode), f"static-{mode}-{alloc.name}")
-                machine.run_cmd(f"mkdir {dest_dir}")
-                machine.put_file(os.path.join(dir_path, filn), dest_dir)
-    return benchs
 
 ################################################################################
 # Application
