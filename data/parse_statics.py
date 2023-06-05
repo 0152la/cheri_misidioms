d16fc8b3c27e6f185b42dec14de6b17a9acc0e14
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2023-06-05 11:35:23 +0100

    Add parsing script

---
+++ data/parse_statics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import pprint
+
+assert(len(sys.argv) == 2)
+with open(sys.argv[1], 'r') as data_json_fd:
+    data = json.load(data_json_fd)[0]
+
+parsed = {}
+for mode, mode_data in data["results_benchs"].items():
+    parsed[mode] = {}
+    for bench in mode_data.keys():
+        parsed[mode][bench] = {"dynamic": {}, "static": {}}
+        print(bench)
+        print(data["results_benchs"][mode][bench]["total-time"])
+        parsed[mode][bench]["dynamic"]["time"] = data["results_benchs"][mode][bench]["total-time"]
+        parsed[mode][bench]["static"]["time"]  = data["results_benchs_static"][mode][bench]["total-time"]
+        print(parsed)
+
+pprint.pprint(parsed)
