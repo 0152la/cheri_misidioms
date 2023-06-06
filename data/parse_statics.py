a08235403c33e7d5d4164a84904df4eb9829575b
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2023-06-06 13:11:12 +0100

    Add

--- data/parse_statics.py
+++ data/parse_statics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import json
 import sys
 import pprint
@@ -8,7 +9,7 @@ def get_both(data, bench, key):
     dynamic = data["results_benchs"][mode][bench][key]
     static  = data["results_benchs_static"][mode][bench][key]
     margin  = 100.00 - static * 100.00 / dynamic
-    return dict.OrderedDict({ "dynamic" : f'{dynamic:,}', "static" : f'{static:,}', "margin" : margin })
+    return collections.OrderedDict({ "dynamic" : f'{dynamic:,}', "static" : f'{static:,}', "margin" : margin })
 
 assert(len(sys.argv) == 2)
 with open(sys.argv[1], 'r') as data_json_fd:
@@ -29,4 +30,4 @@ for mode, mode_data in data["results_benchs"].items():
             parsed[mode][bench]["pmc_cpu_cycles"] = get_both(data, bench, "CPU_CYCLES")
             parsed[mode][bench]["pmc_instr_retired"] = get_both(data, bench, "INST_RETIRED")
 
-pprint.pprint(parsed)
+pprint.pprint(parsed, width = get_terminal_size().columns)
