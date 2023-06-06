eb226f4e13304866f8d3ccb489c7c4b9f1ff725c
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2023-06-06 12:50:46 +0100

    Improve statistics script

--- data/parse_statics.py
+++ data/parse_statics.py
@@ -4,16 +4,29 @@ import json
 import sys
 import pprint
 
+def get_both(data, bench, key):
+    dynamic = data["results_benchs"][mode][bench][key]
+    static  = data["results_benchs_static"][mode][bench][key]
+    margin  = static * 100.00 / dynamic
+    return { "dynamic" : dynamic, "static" : static, "margin" : margin }
+
 assert(len(sys.argv) == 2)
 with open(sys.argv[1], 'r') as data_json_fd:
     data = json.load(data_json_fd)[0]
 
+margin = 7.00
 parsed = {}
 for mode, mode_data in data["results_benchs"].items():
     parsed[mode] = {}
     for bench in mode_data.keys():
-        parsed[mode][bench] = {"dynamic": {}, "static": {}}
-        parsed[mode][bench]["dynamic"]["time"] = data["results_benchs"][mode][bench]["total-time"]
-        parsed[mode][bench]["static"]["time"]  = data["results_benchs_static"][mode][bench]["total-time"]
+        dyn_time = data["results_benchs"][mode][bench]["total-time"]
+        sta_time = data["results_benchs_static"][mode][bench]["total-time"]
+
+        time_improvement = sta_time * 100.00 / dyn_time < 100
+        if  time_improvement < 100 - margin:
+            parsed[mode][bench] = {}
+            parsed[mode][bench]["time"] = {"dynamic" : dyn_time, "static" : sta_time, "margin" : time_improvement}
+            parsed[mode][bench]["pmc_cpu_cycles"] = get_both(data, bench, "CPU_CYCLES")
+            parsed[mode][bench]["pmc_instr_retired"] = get_both(data, bench, "INST_RETIRED")
 
 pprint.pprint(parsed)
